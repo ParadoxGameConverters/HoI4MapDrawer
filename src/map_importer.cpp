e1ff566621e861443252762426b9e5359e9d463d
Idhrendur <idhrendur@hotmail.com>
2022-04-21 20:13:39 -0700

    Scan provinces image

--- src/map_importer.cpp
+++ src/map_importer.cpp
@@ -1,5 +1,7 @@
 #include "src/map_importer.h"
 
+#include "external/CImg/CImg.h"
+
 
 
 namespace hoi4_map_drawer
@@ -18,12 +20,21 @@ namespace
 std::map<int, std::set<Pixel>> GetProvinceDefinitions()
 {
    std::map<int, std::set<Pixel>> definitions;
+   cimg_library::CImg<int32_t> provinces_image(
+       "C:/Program Files (x86)/Steam/steamapps/common/Hearts of Iron IV/map/provinces.bmp");
+   for (int x = 0; x < provinces_image.width(); ++x)
+   {
+      for (int y = 0; y < provinces_image.height(); ++y)
+      {
+         const auto pixel = *provinces_image.data(x, y);
+         const Pixel coords{.x = x, .y = y};
+         if (auto [existing, success] = definitions.emplace(pixel, std::set{coords}); !success)
+         {
+            existing->second.insert({x, y});
+         }
+      }
+   }
 
-   std::set<Pixel> pixels_one{{16, 25}, {196, 225}};
-   definitions.emplace(4, pixels_one);
-
-   std::set<Pixel> pixels_two{{36, 49}, {256, 289}};
-   definitions.emplace(6, pixels_two);
    return definitions;
 }
 
