e8f0ec731a5f43ad2366833d0b6b59457386eed8
Idhrendur <idhrendur@hotmail.com>
2022-04-23 00:40:14 -0700

    Move Pixel to its own file

--- src/map_importer/map_importer.h
+++ src/map_importer/map_importer.h
@@ -1,11 +1,13 @@
-#ifndef SRC_MAP_IMPORTER_H_
-#define SRC_MAP_IMPORTER_H_
+#ifndef SRC_MAP_IMPORTER_MAP_IMPORTER_H_
+#define SRC_MAP_IMPORTER_MAP_IMPORTER_H_
 
 
 
 #include <map>
 #include <set>
 #include <string>
+
+#include "src/map_importer/pixel.h"
 
 
 
@@ -14,21 +16,11 @@ namespace hoi4_map_drawer
 namespace map_importer
 {
 
-
-struct Pixel
-{
-   int x;
-   int y;
-   bool operator<(const Pixel& rhs) const { return y < rhs.y || x < rhs.x; }
-   bool operator==(const Pixel& rhs) const { return y == rhs.y && x == rhs.x; }
-};
-
 std::map<int, std::set<Pixel>> GetProvinceDefinitions(const std::string& hoi4_folder);
-
 
 }  // namespace map_importer
 }  // namespace hoi4_map_drawer
 
 
 
-#endif  // SRC_MAP_IMPORTER_H_
+#endif  // SRC_MAP_IMPORTER_MAP_IMPORTER_H_
