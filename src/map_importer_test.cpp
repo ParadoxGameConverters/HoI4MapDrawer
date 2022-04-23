4fc370f0b53086514975a975ea1e40a6806d9225
Idhrendur <idhrendur@hotmail.com>
2022-04-22 22:34:14 -0700

    Basic setup for tests

---
+++ src/map_importer_test.cpp
@@ -0,0 +1,17 @@
+#include "map_importer.h"
+
+#include <algorithm>
+#include <filesystem>
+#include <fstream>
+#include <iterator>
+#include <sstream>
+
+#include "external/commonItems/OSCompatibilityLayer.h"
+#include "external/googletest/googletest/include/gtest/gtest.h"
+
+
+
+TEST(MapImporterTest, ModFolderIsCleared)
+{
+	EXPECT_TRUE(true);
+}
