1626b9359fc25638b46ef3df4f66f01c1d1356ee
Idhrendur <idhrendur@hotmail.com>
2022-04-25 19:48:32 -0700

    That was it! Is state_importer_test good?

--- src/save_reader/state_importer_test.cpp
+++ src/save_reader/state_importer_test.cpp
@@ -7,60 +7,60 @@
 
 
 
-//TEST(StateImporterTest, IdIsAsSet)
-//{
-//   std::stringstream input;
-//
-//   hoi4_map_drawer::save_reader::StateImporter importer;
-//   const auto state = importer.ImportState(42, input);
-//
-//   EXPECT_EQ(state.GetId(), 42);
-//}
-//
-//
-//TEST(StateImporterTest, GetOwnerReturnsNulloptIfMissingOwner)
-//{
-//   std::stringstream input;
-//
-//   hoi4_map_drawer::save_reader::StateImporter importer;
-//   const auto state = importer.ImportState(0, input);
-//
-//   EXPECT_EQ(state.GetOwner(), std::nullopt);
-//}
-//
-//
-//TEST(StateImporterTest, OwnerCanBeImported)
-//{
-//   std::stringstream input;
-//   input << "= {\n";
-//   input << "\towner=TAG\n";
-//   input << "}";
-//
-//   hoi4_map_drawer::save_reader::StateImporter importer;
-//   const auto state = importer.ImportState(0, input);
-//
-//   ASSERT_TRUE(state.GetOwner());
-//   EXPECT_EQ(state.GetOwner(), "TAG");
-//}
-//
-//
-//TEST(StateImporterTest, ExtraInputIsIgnored)
-//{
-//   std::stringstream log;
-//   std::streambuf* cout_buffer = std::cout.rdbuf();
-//   std::cout.rdbuf(log.rdbuf());
-//
-//   std::stringstream input;
-//   input << "= {\n";
-//   input << "\tunhandled_input = 42\n";
-//   input << "}";
-//
-//   hoi4_map_drawer::save_reader::StateImporter importer;
-//   auto state_data = importer.ImportState(0, input);
-//   auto owner = state_data.GetOwner();  // make the annoying warning go away
-//   owner.reset();
-//
-//   EXPECT_TRUE(log.str().empty());
-//
-//   std::cout.rdbuf(cout_buffer);
-//}
+TEST(StateImporterTest, IdIsAsSet)
+{
+   std::stringstream input;
+
+   hoi4_map_drawer::save_reader::StateImporter importer;
+   const auto state = importer.ImportState(42, input);
+
+   EXPECT_EQ(state.GetId(), 42);
+}
+
+
+TEST(StateImporterTest, GetOwnerReturnsNulloptIfMissingOwner)
+{
+   std::stringstream input;
+
+   hoi4_map_drawer::save_reader::StateImporter importer;
+   const auto state = importer.ImportState(0, input);
+
+   EXPECT_EQ(state.GetOwner(), std::nullopt);
+}
+
+
+TEST(StateImporterTest, OwnerCanBeImported)
+{
+   std::stringstream input;
+   input << "= {\n";
+   input << "\towner=TAG\n";
+   input << "}";
+
+   hoi4_map_drawer::save_reader::StateImporter importer;
+   const auto state = importer.ImportState(0, input);
+
+   ASSERT_TRUE(state.GetOwner());
+   EXPECT_EQ(state.GetOwner(), "TAG");
+}
+
+
+TEST(StateImporterTest, ExtraInputIsIgnored)
+{
+   std::stringstream log;
+   std::streambuf* cout_buffer = std::cout.rdbuf();
+   std::cout.rdbuf(log.rdbuf());
+
+   std::stringstream input;
+   input << "= {\n";
+   input << "\tunhandled_input = 42\n";
+   input << "}";
+
+   hoi4_map_drawer::save_reader::StateImporter importer;
+   auto state_data = importer.ImportState(0, input);
+   auto owner = state_data.GetOwner();  // make the annoying warning go away
+   owner.reset();
+
+   EXPECT_TRUE(log.str().empty());
+
+   std::cout.rdbuf(cout_buffer);
+}
