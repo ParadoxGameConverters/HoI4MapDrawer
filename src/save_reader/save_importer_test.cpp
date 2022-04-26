360cd90e2ceedc3625d8ccc4f0f99eccc07002c1
Idhrendur <idhrendur@hotmail.com>
2022-04-25 20:37:18 -0700

    Enable tests and include rakaly

--- src/save_reader/save_importer_test.cpp
+++ src/save_reader/save_importer_test.cpp
@@ -7,45 +7,45 @@
 
 
 
-// TEST(SaveImporterTest, MissingFileThrowsException)
-//{
-//    hoi4_map_drawer::save_reader::SaveImporter importer;
-//    EXPECT_THROW(importer.ImportSave("./test_data/save_reader/missing_save.hoi4"), std::runtime_error);
-// }
-//
-//
-//TEST(SaveImporterTest, NoStatesFromEmptySave)
-//{
-//   hoi4_map_drawer::save_reader::SaveImporter importer;
-//   const auto save = importer.ImportSave("./test_data/save_reader/empty_save.hoi4");
-//
-//   EXPECT_TRUE(save.GetStates().empty());
-//}
-//
-//
-//TEST(SaveImporterTest, StatesAreImported)
-//{
-//   hoi4_map_drawer::save_reader::SaveImporter importer;
-//   const auto save = importer.ImportSave("./test_data/save_reader/good_save.hoi4");
-//
-//   EXPECT_THAT(save.GetStates(),
-//       testing::UnorderedElementsAre(testing::Pair(1, hoi4_map_drawer::save_reader::State(1, "TAG")),
-//           testing::Pair(2, hoi4_map_drawer::save_reader::State(2, "TWO"))));
-//}
-//
-//
-//TEST(SaveImporterTest, ExtraDataInSaveIsIgnored)
-//{
-//   std::stringstream log;
-//   std::streambuf* cout_buffer = std::cout.rdbuf();
-//   std::cout.rdbuf(log.rdbuf());
-//
-//   hoi4_map_drawer::save_reader::SaveImporter importer;
-//   const auto save = importer.ImportSave("./test_data/save_reader/extra_data_save.hoi4");
-//   auto states = save.GetStates();  // make the annoying warning go away
-//   states.clear();  // make the annoying warning go away
-//
-//   EXPECT_TRUE(log.str().empty());
-//
-//   std::cout.rdbuf(cout_buffer);
-//}
+TEST(SaveImporterTest, MissingFileThrowsException)
+{
+   hoi4_map_drawer::save_reader::SaveImporter importer;
+   EXPECT_THROW(importer.ImportSave("./test_data/save_reader/missing_save.hoi4"), std::runtime_error);
+}
+
+
+TEST(SaveImporterTest, NoStatesFromEmptySave)
+{
+   hoi4_map_drawer::save_reader::SaveImporter importer;
+   const auto save = importer.ImportSave("./test_data/save_reader/empty_save.hoi4");
+
+   EXPECT_TRUE(save.GetStates().empty());
+}
+
+
+TEST(SaveImporterTest, StatesAreImported)
+{
+   hoi4_map_drawer::save_reader::SaveImporter importer;
+   const auto save = importer.ImportSave("./test_data/save_reader/good_save.hoi4");
+
+   EXPECT_THAT(save.GetStates(),
+       testing::UnorderedElementsAre(testing::Pair(1, hoi4_map_drawer::save_reader::State(1, "TAG")),
+           testing::Pair(2, hoi4_map_drawer::save_reader::State(2, "TWO"))));
+}
+
+
+TEST(SaveImporterTest, ExtraDataInSaveIsIgnored)
+{
+   std::stringstream log;
+   std::streambuf* cout_buffer = std::cout.rdbuf();
+   std::cout.rdbuf(log.rdbuf());
+
+   hoi4_map_drawer::save_reader::SaveImporter importer;
+   const auto save = importer.ImportSave("./test_data/save_reader/extra_data_save.hoi4");
+   auto states = save.GetStates();  // make the annoying warning go away
+   states.clear();                  // make the annoying warning go away
+
+   EXPECT_TRUE(log.str().empty());
+
+   std::cout.rdbuf(cout_buffer);
+}
