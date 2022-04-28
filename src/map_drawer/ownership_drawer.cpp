bc61d12ef37bfd79739dafc24b97374cdcb2441f
Idhrendur <idhrendur@hotmail.com>
2022-04-27 23:09:23 -0700

    Extract some functions for clarity

--- src/map_drawer/ownership_drawer.cpp
+++ src/map_drawer/ownership_drawer.cpp
@@ -6,6 +6,65 @@
 #include "external/commonItems/Color.h"
 
 
+
+namespace
+{
+
+cimg_library::CImg<uint8_t> CreateBaseMap(int width, int height)
+{
+   cimg_library::CImg<uint8_t> map(width, height, 1, 3);
+   map.fill(255);
+   return map;
+}
+
+
+commonItems::Color DetermineColor(int state_number,
+    const std::map<int, hoi4_map_drawer::save_reader::State>& save_states,
+    const std::map<std::string, commonItems::Color>& tags_to_colors_map)
+{
+   const auto save_state = save_states.find(state_number);
+   if (save_state == save_states.end())
+   {
+      return commonItems::Color(std::array<int, 3>{0, 0, 0});
+   }
+
+   const auto& owner = save_state->second.GetOwner();
+   if (!owner)
+   {
+      return commonItems::Color(std::array<int, 3>{0, 0, 0});
+   }
+
+   const auto color_mapping = tags_to_colors_map.find(*owner);
+   if (color_mapping == tags_to_colors_map.end())
+   {
+      return commonItems::Color(std::array<int, 3>{0, 0, 0});
+   }
+   return color_mapping->second;
+}
+
+
+void ColorProvince(int province,
+    const std::map<int, std::set<hoi4_map_drawer::map_importer::Pixel>>& map_definitions,
+    const commonItems::Color& color,
+    cimg_library::CImg<uint8_t>& map)
+{
+   const auto& definition = map_definitions.find(province);
+   if (definition == map_definitions.end())
+   {
+      return;
+   }
+
+   for (const auto& pixel: definition->second)
+   {
+      map(pixel.x, pixel.y, 0, 0) = color.r();
+      map(pixel.x, pixel.y, 0, 1) = color.g();
+      map(pixel.x, pixel.y, 0, 2) = color.b();
+   }
+}
+
+}  // namespace
+
+
 void hoi4_map_drawer::map_importer::DrawOwnershipMap(int width,
     int height,
     const std::map<int, std::vector<int>>& state_definitions,
@@ -13,38 +72,14 @@ void hoi4_map_drawer::map_importer::DrawOwnershipMap(int width,
     const std::map<std::string, commonItems::Color>& tags_to_colors_map,
     const std::map<int, std::set<Pixel>>& map_definitions)
 {
-   cimg_library::CImg<uint8_t> ownership_map(width, height, 1, 3);
-   ownership_map.fill(255);
+   auto ownership_map = CreateBaseMap(width, height);
+
    for (const auto& [state_number, state_provinces]: state_definitions)
    {
-      commonItems::Color state_color(std::array<int, 3>{0, 0, 0});
-      const auto save_state = save_states.find(state_number);
-      if (save_state != save_states.end())
-      {
-         const auto& owner = save_state->second.GetOwner();
-         if (owner)
-         {
-            const auto color_mapping = tags_to_colors_map.find(*owner);
-            if (color_mapping != tags_to_colors_map.end())
-            {
-               state_color = color_mapping->second;
-            }
-         }
-      }
+      const auto state_color = DetermineColor(state_number, save_states, tags_to_colors_map);
       for (const auto& province: state_provinces)
       {
-         const auto& definition = map_definitions.find(province);
-         if (definition == map_definitions.end())
-         {
-            continue;
-         }
-
-         for (const auto& pixel: definition->second)
-         {
-            ownership_map(pixel.x, pixel.y, 0, 0) = state_color.r();
-            ownership_map(pixel.x, pixel.y, 0, 1) = state_color.g();
-            ownership_map(pixel.x, pixel.y, 0, 2) = state_color.b();
-         }
+         ColorProvince(province, map_definitions, state_color, ownership_map);
       }
    }
 
