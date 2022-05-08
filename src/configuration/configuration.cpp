757f0a69723709570a0c7ed333934f17c3ab5eee
Idhrendur <idhrendur@hotmail.com>
2022-05-08 11:46:32 -0700

    Add properly-capitalized file back

---
+++ src/configuration/configuration.cpp
@@ -0,0 +1,26 @@
+#include "src/configuration/configuration.h"
+
+#include "external/commonItems/Parser.h"
+#include "external/commonItems/ParserHelpers.h"
+
+
+
+hoi4_map_drawer::configuration::Configuration hoi4_map_drawer::configuration::ImportConfiguration(
+    std::string_view filename)
+{
+   Configuration configuration;
+
+   commonItems::parser configuration_parser;
+   configuration_parser.registerKeyword("hoi4_folder", [&configuration](std::istream& the_stream) {
+      configuration.hoi4_folder = commonItems::getString(the_stream);
+   });
+   configuration_parser.registerKeyword("mod_folder", [&configuration](std::istream& the_stream) {
+      configuration.mod_folder = commonItems::getString(the_stream);
+   });
+   configuration_parser.registerKeyword("save_location", [&configuration](std::istream& the_stream) {
+      configuration.save_location = commonItems::getString(the_stream);
+   });
+   configuration_parser.parseFile(filename);
+
+   return configuration;
+}
