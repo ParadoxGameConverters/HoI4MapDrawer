7ee35d164b2df061d968ef3210c4273973c40088
Idhrendur <idhrendur@hotmail.com>
2022-05-10 20:46:19 -0700

    Fix formatting

--- src/save_reader/faction_importer_test.cpp
+++ src/save_reader/faction_importer_test.cpp
@@ -36,25 +36,25 @@ TEST(FactionImporterTest, MembersCanBeInput)
 
 TEST(FactionImporterTest, ExtraDataIsIgnored)
 {
-    std::stringstream log;
-    std::streambuf* cout_buffer = std::cout.rdbuf();
-    std::cout.rdbuf(log.rdbuf());
+   std::stringstream log;
+   std::streambuf* cout_buffer = std::cout.rdbuf();
+   std::cout.rdbuf(log.rdbuf());
 
-    hoi4_map_drawer::save_reader::FactionImporter faction_importer;
+   hoi4_map_drawer::save_reader::FactionImporter faction_importer;
 
-    std::stringstream input;
-    input << "= {\n";
-    input << "\tideology=democratic\n";
-    input << "\tmembers = {\n";
-    input << "\t\t\"UTA\"\n";
-    input << "\t\t\"CAN\"\n";
-    input << "\t}\n";
-    input << "}";
+   std::stringstream input;
+   input << "= {\n";
+   input << "\tideology=democratic\n";
+   input << "\tmembers = {\n";
+   input << "\t\t\"UTA\"\n";
+   input << "\t\t\"CAN\"\n";
+   input << "\t}\n";
+   input << "}";
 
-    auto mappings = faction_importer.ImportFaction(input);
-    mappings.clear();                  // make the annoying warning go away
+   auto mappings = faction_importer.ImportFaction(input);
+   mappings.clear();  // make the annoying warning go away
 
-    EXPECT_TRUE(log.str().empty());
+   EXPECT_TRUE(log.str().empty());
 
-    std::cout.rdbuf(cout_buffer);
+   std::cout.rdbuf(cout_buffer);
 }
