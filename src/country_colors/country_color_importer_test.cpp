128ee64feec4acb78c61812194f4b7cb9b5a3b23
Idhrendur <idhrendur@hotmail.com>
2022-05-14 19:54:59 -0700

    Use imported cosmetic tags

--- src/country_colors/country_color_importer_test.cpp
+++ src/country_colors/country_color_importer_test.cpp
@@ -13,8 +13,10 @@ TEST(CountryColorImporterTest, CountryColorsCanBeImported)
 {
    const commonItems::ModFilesystem mod_filesystem("test_data/country_colors/country_color_importer/", {});
 
-   const auto colors = hoi4_map_drawer::country_colors::ImportCountryColors(mod_filesystem, {});
+   const auto colors =
+       hoi4_map_drawer::country_colors::ImportCountryColors(mod_filesystem, {{"TST", "TST_cosmetic_tag"}});
    EXPECT_THAT(colors,
        testing::UnorderedElementsAre(testing::Pair("ONE", commonItems::Color(std::array{1, 2, 3})),
-           testing::Pair("TWO", commonItems::Color(std::array{2, 4, 6}))));
+           testing::Pair("TWO", commonItems::Color(std::array{2, 4, 6})),
+           testing::Pair("TST", commonItems::Color(std::array{3, 6, 9}))));
 }
